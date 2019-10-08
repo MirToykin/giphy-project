2da3845105cd6670ea5876022d70194fe55af865
MirToykin <miroslav.toykin@gmail.com>
2019-10-08 23:33:21 +0300

    реализована карусель трендовых gif, стоит еще поработать над анимацией исчезновения и появления кнопок управления карусели

--- js/script.js
+++ js/script.js
@@ -1,16 +1,29 @@
 let apiKey = 'xttvZ1Z1SnbXsD7xJB7kWsX7oTqeqZWY';
 let trendLength = 20;
 let giphyAPI = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=${trendLength}`;
+
 let trendSlidesContainer = document.querySelector('.trending__slides');
+let forwardBtn = document.querySelector('.trending__btn--forward'),
+    backBtn = document.querySelector('.trending__btn--back');
+
 let trendSliderWidth = document.querySelector('.trending__slider-window').offsetWidth;
-let trendSlidesWidths = []; // в каждом элементе массива 
+let visibleSliderWidth = trendSliderWidth;
+let trendSlidesWidths = [];  // в каждом элементе массива 
 //будет храниться ширина картинки + ее margin
+let allSlidesWidth;
+
 let slideMarginRight = 10;
 let slideHeight = 150;
-let sliderOffset = 0;
-let previousOffset = 0;
+
+let forwardOffset = 0,
+    backOffset = 0,
+    lastOffset;
+
 let trendIndex = 0;
 
+let btnTransitionTime = parseFloat(getComputedStyle(document.querySelector('.trending__btn')).transitionDuration) * 1000;
+
+// ___________________________Карусель с трендовыми gif__________________________
 
 function showTrendingGifs() {
   fetch(giphyAPI)
@@ -24,35 +37,132 @@ function showTrendingGifs() {
       let coef = json.data[i].images.original.width/json.data[i].images.original.height;
       trendSlidesWidths.push(coef);
     }
+    
     trendSlidesWidths = trendSlidesWidths.map((item => item * slideHeight + slideMarginRight));
-    document.onclick = function() {
-      scroll(getSliderOffset());
+    
+    allSlidesWidth = trendSlidesWidths.reduce((sum, current) => sum + current, 0);
+    
+    forwardBtn.onclick = function() {
+      scroll(getForwardOffset());
+
+      if (getComputedStyle(backBtn).display == 'none') {
+        backBtn.style.display = 'block';
+        backBtn.style.opacity = '1';
+      }
+    }
+
+    backBtn.onclick = function() {
+      scroll(getBackOffset());
+
+      if (getComputedStyle(forwardBtn).display == 'none') {
+        forwardBtn.style.display = 'block';
+        forwardBtn.style.opacity = '1';
+      }
     }
     
   })
   .catch(err => console.log(err.message))
 }
 
-function getSliderOffset() {
-  for (let i = trendIndex; i < trendSlidesWidths.length; i++) {
-    if (sliderOffset + trendSlidesWidths[i] > trendSliderWidth) {
+function getForwardOffset() {
+  lastOffset = forwardOffset;
+  
+  for (let i = trendIndex; i < trendLength; i++) {
+    
+    if (forwardOffset + trendSlidesWidths[i] > trendSliderWidth) {
+      if (isLastScroll(i - 1, trendSlidesWidths, visibleSliderWidth)) {
 
-      sliderOffset -= trendSlidesWidths[i - 1];
-      trendSliderWidth += sliderOffset - previousOffset;
-      previousOffset = sliderOffset;
-      trendIndex = i - 1;
-      return sliderOffset;
+        forwardOffset = lastOffset;
+        trendSliderWidth = allSlidesWidth;
+        
+        forwardBtn.style.opacity = '0';
+        
+        setTimeout(() => {
+          forwardBtn.style.display = 'none';
+        }, btnTransitionTime);
+        
+        return allSlidesWidth - visibleSliderWidth;
+        
+      } else {
+        
+        forwardOffset -= trendSlidesWidths[i - 1];
+        trendSliderWidth = forwardOffset + visibleSliderWidth;
+        trendIndex = i - 1;
+        return forwardOffset;
+        
+      }
       
-    } else if (i == trendSlidesWidths.length - 1 && sliderOffset + trendSlidesWidths[i] <= trendSliderWidth) {
-
-    }
-    sliderOffset += trendSlidesWidths[i];
+    } 
+    forwardOffset += trendSlidesWidths[i];
   }
 }
+
+function getBackOffset() {
+  let delIndex = trendIndex;
+  
+  // if (forwardOffset == 0) return;
+  
+  if (trendSliderWidth == allSlidesWidth) { // последний слайд по 
+  //правому краю окна слайдера
+    trendSliderWidth = forwardOffset + visibleSliderWidth;
+    return lastOffset;
+  }
+  
+  for (let i = trendIndex; i >= 0; i--) {
+    
+    if (backOffset + trendSlidesWidths[i] > visibleSliderWidth) {
+
+      backOffset -= trendSlidesWidths[delIndex];
+      forwardOffset -= backOffset;
+      trendSliderWidth = forwardOffset + visibleSliderWidth;
+      trendIndex = i + 1;
+      backOffset = 0;
+      return forwardOffset;
+
+    } else if (i == 0 && backOffset + trendSlidesWidths[i] <= visibleSliderWidth ) {
+
+      forwardOffset = 0;
+      trendSliderWidth = visibleSliderWidth;
+      backOffset = 0;
+      trendIndex = 0;
+      
+      backBtn.style.opacity = '0';
+      
+      setTimeout(() => {
+        backBtn.style.display = 'none';
+      }, btnTransitionTime)
+      
+      return forwardOffset;
+
+    }
+    backOffset += trendSlidesWidths[i];
+  }
+  
+}
+
+function isLastScroll(index, iterable, width) {
+  if (index < 0) return;
+  
+  let iterableWidth = 0;
+  
+  for (let i = index; i < iterable.length; i++) {
+    iterableWidth += iterable[i];
+  }
+  
+  if (iterableWidth <= width) {
+    return true;
+  }
+  
+  return false;
+}
+
 function scroll(offset) {
   trendSlidesContainer.style.left = -offset + 'px';
 }
 showTrendingGifs();
 
-// ___________________________поисковая выдача__________________________
+// ___________________________Поисковая выдача__________________________
 
+
+// идея: вынести кнопки управления за окноо слайдера, при наведении делать их фон полупрозрачным, 
+// чтобы показать часть следующего изображения, возможно придется сузить окно слайдера
